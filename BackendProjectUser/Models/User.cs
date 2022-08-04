287746a5842b5d336b04e34c9b94463683f3e6c1
LEIDY <leidy-ktpm@hotmail.com>
2022-08-04 14:22:54 -0500

    Agregar archivos de proyecto.

---
+++ BackendProjectUser/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BackendProjectUser.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Column( TypeName = "varchar(255)")]
+        public string Name { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(255)")]
+        public string Lastname { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(255)")]
+        public string Phone { get; set; }
+
+    }
+}
